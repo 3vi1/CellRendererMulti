1c1c0ec7cebb990de901e91ee3a6c60d2c7c611f
Jesse Litton <git@eternaldusk.com>
2014-06-02 15:44:50 -0500

    CellRenderers, How Do They Work?

---
+++ CellRendererMulti/CellRendererMulti.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Diagnostics;
+
+using Gdk;
+using Gtk;
+
+namespace EternalDusk
+{
+	public class CellRendererMulti : CellRenderer
+	{
+		public CellType CellType	{ get; set; }
+		public bool Editable 		{ get; set; }
+		public EditedHandler Edited	{ get; set; }
+		public TreeModel Model 		{ get; set; }
+		public int TextColumn		{ get; set; }
+
+		[GLib.Property ("text")]
+		public string Text 		{ get; set; }
+
+		public override void GetSize (
+			Widget widget,
+			ref Rectangle cell_area,
+			out int x_offset,
+			out int y_offset,
+			out int width,
+			out int height)
+		{
+			base.GetSize (widget, ref cell_area, out x_offset, out y_offset, out width, out height);
+			width = 150;
+		}
+
+		public override CellEditable StartEditing(
+			Event editEvent,
+			Widget widget,
+			string path,
+			Rectangle background,
+			Rectangle cellArea,
+			CellRendererState flags
+		) {
+			return Renderer.StartEditing(editEvent, widget, path, background, cellArea, flags);
+		}
+
+		private CellRenderer Renderer
+		{
+			get 
+			{
+				if (CellType == CellType.Combo)
+				{
+					return comboRenderer;
+				}
+
+				return textRenderer; 
+			}
+		}
+
+		protected override void Render(
+			Gdk.Drawable window,		// Note the type! --3vi1
+			Widget widget,
+			Rectangle background_area,
+			Rectangle cell_area,
+			Rectangle expose_area,
+			CellRendererState flags)
+		{
+			// Cast window to correct type, or you won't call correct renderer! --3vi1
+			Renderer.Render(
+				(Gdk.Window) window,
+				widget,
+				background_area,
+				cell_area,
+				expose_area,
+				flags
+			);
+		}
+
+		private CellRendererText textRenderer;
+		private CellRendererCombo comboRenderer;
+
+		public ListStore comboChoices;
+
+		public CellRendererMulti() : base()
+		{
+			// Build Combo Options
+			comboChoices = new ListStore (typeof(string));
+			comboChoices.AppendValues("Choice A");
+			comboChoices.AppendValues("Choice B");
+			comboChoices.AppendValues("Choice C");
+
+			Model = comboChoices;
+			TextColumn = 0;
+			Mode = CellRendererMode.Editable;
+		}
+
+		public void Update()
+		{
+			switch (CellType)
+			{
+			case CellType.Combo:
+				comboRenderer = new CellRendererCombo();
+				comboRenderer.Model = Model;
+				comboRenderer.TextColumn = TextColumn;
+				comboRenderer.Editable = Editable;
+				foreach (EditedHandler handler in Edited.GetInvocationList())
+				{
+					comboRenderer.Edited += handler;
+				}
+				comboRenderer.Text = Text;
+				break;
+			case CellType.Text:
+				textRenderer = new CellRendererText();
+				textRenderer.Editable = Editable;
+				foreach (EditedHandler handler in Edited.GetInvocationList())
+				{
+					textRenderer.Edited += handler;
+				}
+				textRenderer.Text = Text;
+				break;
+			}
+			//Renderer.Mode = CellRendererMode.Activatable;
+		}
+	}
+}
+
---
+++ CellRendererMulti/MainWindow.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Gtk;
+using EternalDusk;
+
+public enum Columns
+{
+	Attribute = 0,
+	Value = 1,
+	CellType = 2
+}
+
+/// <summary>
+/// These are the types of cells our renderer supports.
+/// </summary>
+public enum CellType
+{
+	Combo,
+	Text
+}
+	
+[TreeNode (ListOnly=true)]
+public class MyTreeNode : TreeNode {
+
+	[TreeNodeValue (Column=(int)Columns.Attribute)]
+	public string MyAttribute;
+
+	[TreeNodeValue (Column=(int)Columns.Value)]
+	public string MyValue;
+
+	// Note: This is a hidden column (not added to view).  --3vi1
+	[TreeNodeValue (Column=(int)Columns.CellType)]
+	public CellType ValueType;
+
+	public MyTreeNode (string newAttribute, string newValue, CellType valueType)
+	{
+		MyAttribute = newAttribute;
+		MyValue = newValue;
+		ValueType = valueType;
+	}
+}
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow() : base(Gtk.WindowType.Toplevel)
+	{
+		Build();
+
+		// Add our Attribute column.
+		nodeview1.AppendColumn("Attribute", new CellRendererText(), "text", (int)Columns.Attribute);
+
+		// Define some test types that will be in the dropdown combo.
+		ListStore storeTypes = new ListStore (typeof(string));
+		storeTypes.AppendValues("test1");
+		storeTypes.AppendValues("test2");
+		storeTypes.AppendValues("test3");
+
+		// Add our Value Column
+		//CellRendererCombo myRenderer = new CellRendererCombo();
+		CellRendererMulti myRenderer = new CellRendererMulti();
+		myRenderer.Model = storeTypes;
+		myRenderer.TextColumn = 0;
+		myRenderer.Editable = true;
+		myRenderer.Edited += new EditedHandler(TypeEdited);
+		TreeViewColumn column = nodeview1.AppendColumn (
+			"Value",
+			myRenderer,
+			"text",
+			(int)Columns.Value
+		);
+		column.SetCellDataFunc(myRenderer, MyCellDataFunc);
+
+		// Note:  Do not add the hidden column that determines cell type. --3vi1
+
+		// Attach the view to the data model/store.
+		nodeview1.NodeStore = Store;
+	}
+
+	// Our data store.
+	NodeStore store;
+	NodeStore Store {
+		get {
+			if (store == null) {
+				store = new NodeStore (typeof (MyTreeNode));
+				store.AddNode (new MyTreeNode ("Text 1", "Combo 1", CellType.Combo));
+				store.AddNode (new MyTreeNode ("Text 2", "Combo 2", CellType.Combo));
+				store.AddNode (new MyTreeNode ("Text 3", "Text 4", CellType.Text));
+			}
+			return store;
+		}
+	}
+
+	private void MyCellDataFunc(
+		TreeViewColumn column,
+		CellRenderer cell,
+		TreeModel model,
+		TreeIter iter
+	) {
+		string cellValue = (string) model.GetValue (iter, (int)Columns.Value);
+		CellType cellType = (CellType) model.GetValue (iter, (int)Columns.CellType);
+		(cell as CellRendererMulti).CellType = cellType;
+		(cell as CellRendererMulti).Text = cellValue;
+		(cell as CellRendererMulti).Update();
+		//column.SetAttributes(cell, "text", (int)Columns.Value);
+		//column.PackStart(cell, true);
+	}
+
+	protected void TypeEdited (object sender, EditedArgs args)
+	{
+		TreeIter iter;
+		/*if (store.GetIterFromString (out iter, args.Path)) {
+			foreach (string name in Enum.GetNames (typeof (TriggerType))) {
+				if (args.NewText == name) {
+					store.SetValue (iter, colTypeIndex, args.NewText);
+					EmitContentChanged ();
+					return;
+				}
+			}
+			string oldText = store.GetValue (iter, colTypeIndex) as string;
+			(sender as CellRendererText).Text = oldText;
+		}*/
+	}
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+}
